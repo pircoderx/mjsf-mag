56da752d7eeea1e60a3ce7be6b5433843068b555
Artem Pirunov <psycoderx@gmail.com>
2024-10-24 10:30:58 +0300

    Lab 2 (part 1)

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Button from './components/Button.vue';
+import { ref } from 'vue';
+const count = ref(0);
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" submsg="gr.8.1213-1 Pirunov Artem Sergiyovych" />
+  <Button label="Button A" color="red" size="middle" icon="vite.svg" />
+  <Button :label="'Count: ' + count" color="green" size="big"
+          @push="++count" />
+  <Button label="Button C" color="blue" size="small" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,33 @@
+<script setup>
+const props = defineProps({
+  label: { type: String, default: 'Button' },
+  color: { type: String, default: 'red' },
+  size: { type: String, default: 'middle' },
+  icon: { type: String }
+});
+defineEmits(['push']);
+function get_style() {
+  const htab = {'small': 'h-7', 'middle': 'h-14', 'big': 'h-28'};
+  const ctab = {
+    'red': ['bg-red-700', 'border-red-900'],
+    'green': ['bg-green-700', 'border-green-900'],
+    'blue': ['bg-blue-700', 'border-blue-900']
+  };
+  const height = Object.hasOwn(htab, props.size)
+                 ? htab[props.size] : 'h-14';
+  const bg = Object.hasOwn(ctab, props.color)
+             ? ctab[props.color][0] : 'bg-red-700';
+  const border = Object.hasOwn(ctab, props.color)
+               ? ctab[props.color][1] : 'border-red-900';
+  return 'content-center text-center text-white border-2 w-full'
+       + ' ' + height + ' ' + bg + ' ' + border;
+}
+</script>
+
+<template>
+  <button @click="$emit('push')" type="button" :class="get_style()">
+    <img v-if="icon" class="inline-block object-scale-down max-h-full m-auto" :src="icon" />
+    <div class="inline-block">{{label}}</div>
+  </button>
+</template>
+
